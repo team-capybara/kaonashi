feebd8d0dd3f6aecfa3291c0a70f875ee6528913
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-05-07 23:22:28 +0900

    chore: add coroutines dependency and create CommonFlow

--- composeApp/src/commonMain/kotlin/Platform.kt
+++ composeApp/src/commonMain/kotlin/Platform.kt
@@ -1,5 +1,24 @@
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
 interface Platform {
     val name: String
 }
 
 expect fun getPlatform(): Platform
+
+/**
+ * For supporting coroutines
+ * @link{Flow} on both platforms.
+ */
+expect class CommonFlow<T>(flow: Flow<T>) : Flow<T>
+
+fun <T> Flow<T>.toCommonFlow() = CommonFlow(this)
+
+/**
+ * For supporting coroutines
+ * @link{StateFlow} on both platforms.
+ */
+expect open class CommonStateFlow<T>(flow: StateFlow<T>) : StateFlow<T>
+
+fun <T> StateFlow<T>.toCommonStateFlow() = CommonStateFlow(this)

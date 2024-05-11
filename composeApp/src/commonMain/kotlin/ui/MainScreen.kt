955be555fd21d8e2344c5fee0cc83b5144b7791d
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-05-11 19:02:08 +0900

    refactor: remove unused imports

--- composeApp/src/commonMain/kotlin/ui/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/MainScreen.kt
@@ -1,25 +1,16 @@
 package ui
 
-import ui.component.Greeting
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.multiplatform.webview.web.WebStateSaver
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewState
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import moime.composeapp.generated.resources.Res
-import moime.composeapp.generated.resources.compose_multiplatform
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun MainScreen() {

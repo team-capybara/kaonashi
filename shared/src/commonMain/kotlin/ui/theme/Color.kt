3c6848aa1ebbd2e6477a323e854397d171d7bd29
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-06-12 01:04:09 +0900

    chore: define Theme with Colors and Typo

---
+++ shared/src/commonMain/kotlin/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Gray50 = Color(0xFFFFFFFF)
+val Gray100 = Color(0xFFF4F6F7)
+val Gray200 = Color(0xFFECECED)
+val Gray300 = Color(0xFFD9D9D9)
+val Gray500 = Color(0xFF3C3C3E)
+val Gray600 = Color(0xFF353535)
+val Gray700 = Color(0xFF262628)
+val Gray800 = Color(0xFF1E1E1E)
+val Gray900 = Color(0xFF191919)
+
+val MoimeGreen = Color(0xFF00E86B)
+val MoimeYellow = Color(0xFFFFD028)
+val MoimeRed = Color(0xFFF13D3D)
+
+val MoimeGradientGreenStart = Color(0xFF25Ff89)
+val MoimeGradientGreenEnd = Color(0xFF00E8BE)

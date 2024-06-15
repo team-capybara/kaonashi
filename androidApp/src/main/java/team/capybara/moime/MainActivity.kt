334a8b3e307a9ca5f22c7b77eebb0da27eb37126
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-06-15 21:10:01 +0900

    feat: set system bar background color

--- androidApp/src/main/java/team/capybara/moime/MainActivity.kt
+++ androidApp/src/main/java/team/capybara/moime/MainActivity.kt
@@ -2,16 +2,21 @@ package team.capybara.moime
 
 import App
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.mmk.kmpnotifier.extensions.onCreateOrOnNewIntent
 import com.mmk.kmpnotifier.notification.NotifierManager
 import com.mmk.kmpnotifier.permission.permissionUtil
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSystemBarStyle()
         initNotifier()
         setContent {
             App()
@@ -21,6 +26,17 @@ class MainActivity : ComponentActivity() {
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         NotifierManager.onCreateOrOnNewIntent(intent)
+    }
+
+    private fun setSystemBarStyle() {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.dark(
+                Color.TRANSPARENT
+            )
+        )
     }
 
     private fun initNotifier() {

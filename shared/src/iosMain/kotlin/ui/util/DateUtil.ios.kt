840beefba9b5e0d1bef6485290b5483a851a4c15
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-06-23 02:27:50 +0900

    fix: LocalDateTime format util on ios

--- shared/src/iosMain/kotlin/ui/util/DateUtil.ios.kt
+++ shared/src/iosMain/kotlin/ui/util/DateUtil.ios.kt
@@ -1,14 +1,15 @@
 package ui.util
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toNSDate
 import platform.Foundation.NSCalendar
 import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSDate
 
 actual fun LocalDateTime.format(format: String): String {
     val dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = format
-    return dateFormatter.stringFromDate(
-        toNSDate(NSCalendar.currentCalendar)
-            ?: throw IllegalStateException("Could not convert kotlin date to NSDate $this")
-    )
+    return dateFormatter.stringFromDate(toInstant(TimeZone.currentSystemDefault()).toNSDate()
 }

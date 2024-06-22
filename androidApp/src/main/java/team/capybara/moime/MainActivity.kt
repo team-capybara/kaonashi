437a23a1a96ce14fceaa973ebb3348d9568ab080
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-06-23 02:55:24 +0900

    fix: apply safe area on drawing

--- androidApp/src/main/java/team/capybara/moime/MainActivity.kt
+++ androidApp/src/main/java/team/capybara/moime/MainActivity.kt
@@ -2,12 +2,13 @@ package team.capybara.moime
 
 import App
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.mmk.kmpnotifier.extensions.onCreateOrOnNewIntent
 import com.mmk.kmpnotifier.notification.NotifierManager
 import com.mmk.kmpnotifier.permission.permissionUtil
@@ -31,10 +32,10 @@ class MainActivity : ComponentActivity() {
     private fun setSystemBarStyle() {
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(
-                Color.TRANSPARENT
+                TOP_APP_BAR_COLOR.toArgb()
             ),
             navigationBarStyle = SystemBarStyle.dark(
-                Color.TRANSPARENT
+                BOTTOM_NAV_BAR_COLOR.toArgb()
             )
         )
     }
@@ -44,5 +45,10 @@ class MainActivity : ComponentActivity() {
         notificationPermissionUtil.askNotificationPermission()
 
         NotifierManager.onCreateOrOnNewIntent(intent)
+    }
+
+    companion object {
+        private val TOP_APP_BAR_COLOR = Color(0xE5292929)
+        private val BOTTOM_NAV_BAR_COLOR = Color(0xD91E1E1E)
     }
 }

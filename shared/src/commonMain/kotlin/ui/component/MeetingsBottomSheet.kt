96a196f19eff76103300ed79192ad2c01cf51548
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-07-14 21:40:08 +0900

    chore: remove title text on meeting bottomsheet

--- shared/src/commonMain/kotlin/ui/component/MeetingsBottomSheet.kt
+++ shared/src/commonMain/kotlin/ui/component/MeetingsBottomSheet.kt
@@ -1,23 +1,15 @@
 package ui.component
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import dev.icerock.moko.resources.compose.fontFamilyResource
-import dev.icerock.moko.resources.compose.stringResource
-import team.capybara.moime.SharedRes
 import ui.model.Meeting
-import ui.theme.Gray50
 import ui.theme.Gray800
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,13 +32,6 @@ fun MeetingsBottomSheet(
                     .padding(bottom = 24.dp)
             )
         ) {
-            Text(
-                text = stringResource(SharedRes.strings.today_meetings),
-                fontFamily = fontFamilyResource(SharedRes.fonts.pretendard_semibold),
-                fontSize = 16.sp,
-                color = Gray50
-            )
-            Spacer(Modifier.height(12.dp))
             meetings.forEach {
                 MoimeMeetingCard(
                     meeting = it,

fd143d3a31fbb0583c90f56b60261af492e98e34
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-08-04 18:01:16 +0900

    feat: implement friend add screen UI

---
+++ shared/src/commonMain/kotlin/ui/friend/InvitationType.kt
@@ -0,0 +1,27 @@
+package ui.friend
+
+import dev.icerock.moko.resources.ImageResource
+import dev.icerock.moko.resources.StringResource
+import team.capybara.moime.SharedRes
+
+enum class InvitationType(
+    val imageResource: ImageResource,
+    val stringResource: StringResource
+) {
+    Instagram(
+        SharedRes.images.img_instagram,
+        SharedRes.strings.instagram
+    ),
+    Facebook(
+        SharedRes.images.img_facebook,
+        SharedRes.strings.facebook
+    ),
+    Kakaotalk(
+        SharedRes.images.img_kakaotalk,
+        SharedRes.strings.kakaotalk
+    ),
+    Etc(
+        SharedRes.images.img_etc,
+        SharedRes.strings.etc
+    )
+}

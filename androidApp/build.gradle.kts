5562ad92cbd4d46c4cca5c9e38e9cde98ad0b0a6
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-09-08 19:25:02 +0900

    chore: bump kotlin, agp, compose-plugin

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.googleServices)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {

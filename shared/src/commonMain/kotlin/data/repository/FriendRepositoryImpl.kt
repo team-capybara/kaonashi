15d4a1766900f7ca021a72a95c9bc9a57c7144bd
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-09-15 23:51:54 +0900

    feat: add action onDismiss on MoimeTextField

--- shared/src/commonMain/kotlin/data/repository/FriendRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/data/repository/FriendRepositoryImpl.kt
@@ -1,21 +1,54 @@
 package data.repository
 
+import data.Api
+import data.model.FriendsResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import ui.model.CursorData
+import ui.model.CursorRequest
 import ui.model.Friend
-import ui.model.dummyFriends
 import ui.repository.FriendRepository
 
 class FriendRepositoryImpl(private val httpClient: HttpClient) : FriendRepository {
 
     override suspend fun getFriendCount(): Int {
-        return 15
+        return httpClient.get(Api.FRIENDS_COUNT).body<Int>()
     }
 
-    override suspend fun getMyFriends(): List<Friend> {
-        return dummyFriends
+    override suspend fun getMyFriends(cursor: CursorRequest, nickname: String?): CursorData<Friend> {
+        return httpClient.get(Api.FRIENDS_FOLLOWINGS) {
+            url {
+                with(cursor) {
+                    cursorId?.let { parameters.append("cursorId", it.toString()) }
+                    limit?.let { parameters.append("size", it.toString()) }
+                }
+                nickname?.let { parameters.append("keyword", it) }
+            }
+        }.body<FriendsResponse>().run {
+            CursorData(
+                data = data.map { it.toUiModel() },
+                nextCursorId = cursorId?.cursorId,
+                isLast = last
+            )
+        }
     }
 
-    override suspend fun getRecommendedFriends(): List<Friend> {
-        return dummyFriends
+    override suspend fun getRecommendedFriends(cursor: CursorRequest, nickname: String?): CursorData<Friend> {
+        return httpClient.get(Api.FRIENDS_RECOMMENDED) {
+            url {
+                with(cursor) {
+                    cursorId?.let { parameters.append("cursorId", it.toString()) }
+                    limit?.let { parameters.append("size", it.toString()) }
+                }
+                nickname?.let { parameters.append("keyword", it) }
+            }
+        }.body<FriendsResponse>().run {
+            CursorData(
+                data = data.map { it.toUiModel() },
+                nextCursorId = cursorId?.cursorId,
+                isLast = last
+            )
+        }
     }
 }

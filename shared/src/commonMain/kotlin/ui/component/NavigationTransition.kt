44c23324010980345e94f7a6424b992ea7b1cdd9
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-10-01 22:15:18 +0900

    chore: define screen navigation transition classes

---
+++ shared/src/commonMain/kotlin/ui/component/NavigationTransition.kt
@@ -0,0 +1,43 @@
+package ui.component
+
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
+import androidx.compose.ui.unit.IntOffset
+import cafe.adriel.voyager.core.annotation.ExperimentalVoyagerApi
+import cafe.adriel.voyager.core.stack.StackEvent
+import cafe.adriel.voyager.transitions.ScreenTransition
+
+@OptIn(ExperimentalVoyagerApi::class)
+class FadeTransition : ScreenTransition {
+
+    override fun enter(lastEvent: StackEvent): EnterTransition {
+        return fadeIn(tween(500, delayMillis = 500))
+    }
+
+    override fun exit(lastEvent: StackEvent): ExitTransition {
+        return fadeOut(tween(500))
+    }
+}
+
+@OptIn(ExperimentalVoyagerApi::class)
+class SlideTransition : ScreenTransition {
+
+    override fun enter(lastEvent: StackEvent): EnterTransition {
+        return slideIn { size ->
+            val x = if (lastEvent == StackEvent.Pop) -size.width else size.width
+            IntOffset(x = x, y = 0)
+        }
+    }
+
+    override fun exit(lastEvent: StackEvent): ExitTransition {
+        return slideOut { size ->
+            val x = if (lastEvent == StackEvent.Pop) size.width else -size.width
+            IntOffset(x = x, y = 0)
+        }
+    }
+}

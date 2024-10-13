183e689b63bdb2f1b3900ea178dd40bc74492e60
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-10-13 16:55:42 +0900

    feat: set action on close webView

--- shared/src/commonMain/kotlin/ui/meeting/detail/DetailScreen.kt
+++ shared/src/commonMain/kotlin/ui/meeting/detail/DetailScreen.kt
@@ -14,6 +14,7 @@ import com.russhwolf.settings.Settings
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import ui.component.MoimeWebView
+import ui.jsbridge.ACCESS_TOKEN_KEY
 import ui.jsbridge.PopHandler
 import ui.meeting.camera.CameraScreen
 import ui.model.Meeting
@@ -27,7 +28,7 @@ data class DetailScreen(private val meeting: Meeting) : Screen, KoinComponent {
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         val screenModel = rememberScreenModel { DetailScreenModel(meeting) }
-        val popHandler = PopHandler(onPop = { navigator.pop() })
+        val popHandler = PopHandler { navigator.pop() }
         val state by screenModel.state.collectAsState()
 
         LaunchedEffect(state) {
@@ -39,7 +40,7 @@ data class DetailScreen(private val meeting: Meeting) : Screen, KoinComponent {
 
         MoimeWebView(
             url = screenModel.webViewUrl,
-            accessToken = settings.getString("accessToken", ""),
+            accessToken = settings.getString(ACCESS_TOKEN_KEY, ""),
             jsMessageHandlers = listOf(screenModel.CameraJsMessageHandler(), popHandler)
         )
     }

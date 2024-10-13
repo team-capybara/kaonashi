e4ccb262520a233f1c5df6e1102ae81014fb96f4
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-10-13 16:14:51 +0900

    feat: implement common webView component

--- shared/src/commonMain/kotlin/ui/login/LoginScreen.kt
+++ shared/src/commonMain/kotlin/ui/login/LoginScreen.kt
@@ -1,24 +1,19 @@
 package ui.login
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import cafe.adriel.voyager.core.screen.uniqueScreenKey
 import cafe.adriel.voyager.koin.koinScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import com.multiplatform.webview.jsbridge.rememberWebViewJsBridge
-import com.multiplatform.webview.web.WebView
-import com.multiplatform.webview.web.rememberWebViewState
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import ui.component.MoimeImagePicker
-import ui.component.SafeAreaColumn
+import ui.component.MoimeWebView
 import ui.jsbridge.ImagePickerResponse
 import ui.main.MainScreen
 import ui.onboarding.OnboardingScreen
@@ -32,16 +27,7 @@ class LoginScreen : Screen {
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         val screenModel = koinScreenModel<LoginScreenModel>()
-        val webviewState = rememberWebViewState(LoginScreenModel.WEBVIEW_LOGIN_URL)
-        val jsBridge = rememberWebViewJsBridge()
         val loginState by screenModel.state.collectAsState()
-
-        LaunchedEffect(jsBridge) {
-            with(jsBridge) {
-                register(screenModel.LoginJsMessageHandler())
-                register(screenModel.imagePickerHandler)
-            }
-        }
 
         LaunchedEffect(loginState) {
             when (val state = loginState) {
@@ -58,13 +44,14 @@ class LoginScreen : Screen {
             }
         }
 
-        SafeAreaColumn {
-            WebView(
-                state = webviewState,
-                webViewJsBridge = jsBridge,
-                modifier = Modifier.fillMaxSize()
+        MoimeWebView(
+            url = LoginScreenModel.WEBVIEW_LOGIN_URL,
+            accessToken = null,
+            jsMessageHandlers = listOf(
+                screenModel.LoginJsMessageHandler(),
+                screenModel.imagePickerHandler
             )
-        }
+        )
 
         (loginState as? LoginScreenModel.State.InProgress)?.onImagePicked?.let { callback ->
             MoimeImagePicker(onPicked = { images ->

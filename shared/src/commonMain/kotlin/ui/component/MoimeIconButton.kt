008add05f79579f2b19b4f3e28b7f8d607ab7d0c
Yeojun Yoon <yjyoon.dev@gmail.com>
2024-10-23 20:08:59 +0900

    feat: enable to set badge on MoimeIconButton

--- shared/src/commonMain/kotlin/ui/component/MoimeIconButton.kt
+++ shared/src/commonMain/kotlin/ui/component/MoimeIconButton.kt
@@ -1,11 +1,16 @@
 package ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
@@ -13,26 +18,40 @@ import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import ui.theme.Gray50
+import ui.theme.MoimeRed
 
 @Composable
 fun MoimeIconButton(
     iconRes: DrawableResource,
     tint: Color = Gray50,
     size: Dp = 24.dp,
+    hasBadge: Boolean = false,
     modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
+
     CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides Dp.Unspecified) {
         IconButton(
             onClick = onClick,
             modifier = modifier,
         ) {
-            Icon(
-                painterResource(iconRes),
-                contentDescription = null,
-                modifier = Modifier.size(size),
-                tint = tint
-            )
+            Box {
+                Icon(
+                    painterResource(iconRes),
+                    contentDescription = null,
+                    modifier = Modifier.size(size).align(Alignment.Center),
+                    tint = tint
+                )
+                if (hasBadge) {
+                    Box(
+                        modifier = Modifier
+                            .padding(end = 2.dp)
+                            .background(MoimeRed, CircleShape)
+                            .size(8.dp)
+                            .align(Alignment.TopEnd)
+                    )
+                }
+            }
         }
     }
 }
